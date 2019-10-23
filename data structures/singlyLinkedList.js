7ae717a6ba05ae6abbb7fa2f11e3d7eaf1bfe681
Konrad <swiercz.konrad@gmail.com>
2019-10-23 23:51:34 +0200

    Singly Linked List done

--- data structures/singlyLinkedList.js
+++ data structures/singlyLinkedList.js
@@ -1,11 +1,11 @@
 class Node {
   constructor(val) {
-    this.val = val;
+    this.value = val;
     this.next = null;
   }
 }
 
-class SinglyLinkedList {
+export class SinglyLinkedList {
   constructor() {
     this._head = null;
     this._tail = null;
@@ -56,29 +56,110 @@ class SinglyLinkedList {
       return null;
     }
 
-    const prevHead = this._head;
-    this._head = prevHead.next;
-
-    prevHead.next = null;
+    const prev = this._head;
+    this._head = prev.next;
+    prev.next = null;
     this._length--;
 
-    return prevHead;
+    return prev;
   }
 
   unshift(val) {
     const node = new Node(val);
+
     if (!this._head) {
       this._tail = node;
     }
+
     node.next = this._head;
     this._head = node;
     this._length++;
 
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this._length) {
+      return null;
+    }
+
+    let el = this._head;
+    let count = 0;
+    while (count !== index) {
+      el = el.next;
+      count++;
+    }
+
+    return el;
+  }
+
+  set(index, val) {
+    const el = this.get(index);
+    if (!el) {
+      return false;
+    }
+    el.value = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this._length) {
+      return false;
+    }
+
+    if (index === 0) {
+      return !!this.unshift(val);
+    }
+
+    if (index === this._length) {
+      return !!this.push(val);
+    }
+
+    const node = new Node(val);
+    const prev = this.get(index - 1);
+    node.next = prev.next;
+    prev.next = node;
+    this._length++;
+
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this._length) {
+      return null;
+    }
+
+    if (index === 0) {
+      return this.shift(index);
+    }
+
+    if (index === this._length - 1) {
+      return this.pop(index);
+    }
+
+    const prev = this.get(index - 1);
+    const removed = prev.next;
+    prev.next = removed.next;
+    this._length--;
+
+    return removed;
+  }
+
+  reverse() {    
+    let node = this._head;
+    this._head = this._tail;
+    this._tail = node;
+
+    let next = null;
+    let prev = null;
+
+    while (node) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this;
+  }
 }
-
-const list = new SinglyLinkedList();
-
-list.push(11);
-list.pop();
