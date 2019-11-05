c92fafdfe41d97cac861057f0f76dc21ecbdaf36
Konrad Świercz <konrad.swiercz@daftcode.pl>
2019-11-05 11:20:11 +0100

    Merge

--- recursion/fib.js
+++
@@ -1,9 +0,0 @@
-function fib(n){
-  if (n <= 2) return 1;
-  return fib(n-1) + fib(n-2);
-}
-
-console.log(fib(4)) // 3
-console.log(fib(10)) // 55
-console.log(fib(28)) // 317811
-console.log(fib(35)) // 9227465
